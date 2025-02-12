44cd65641e5e0708cdfabba28eae4beb0e3f686b
vicenzoli <162443822+vicenzoli@users.noreply.github.com>
2025-02-12 10:11:08 -0300

    Add files via upload

---
+++ Nova pasta/atividade3.js
@@ -0,0 +1,18 @@
+let compra = 100
+var desconto = "DESC10" & "DESC20"
+
+switch (compra) {
+    case'DESC10':
+    console.log(compra * 0.9)
+    break
+
+case'DESC20':
+console.log(compra * 0.8)
+break
+
+default: console.log("nada")
+}
+
+  
+ 
+    
---
+++ Nova pasta/atividade4.js
@@ -0,0 +1,13 @@
+let numero = 0
+let saberNumero = numero === 
+
+(numero === 0 ? "retorne ao zero" : "retorne ao zero") 
+    console.log("retorne ao zero")
+
+
+let SaberNumero2 = numero % 2 === 0
+
+                    ? (numero > 0 ? "par positivo": "par negativo")
+                    : (numero > 0 ? "impar positivo": "impar negativo")
+
+console.log(saberNumero2)
